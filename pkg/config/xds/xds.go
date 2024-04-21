5f11e91fc461cde08eb9122a27bf21a94bb60565
Mark4z <36187602+mark4z@users.noreply.github.com>
2024-04-21 15:19:45 +0800

    remove istio (#622)
    * Move Pixiu com files in /cmd/pixiu to pkg/cmd
    
    * Move Pixiu com files in /cmd/pixiu to pkg/cmd
    
    * remove all istio stuff
    
    * remove all istio stuff
    
    * remove all istio stuff

--- pkg/config/xds/xds.go
+++ pkg/config/xds/xds.go
@@ -1,94 +1,170 @@
-// Copyright Istio Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package xds
 
 import (
-	"bytes"
-	"errors"
-	"fmt"
+	"sync"
 )
 
 import (
-	bootstrapv3 "github.com/envoyproxy/go-control-plane/envoy/config/bootstrap/v3"
-	cluster "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
-	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
-	route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
-	httpConn "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
-	"github.com/golang/protobuf/jsonpb" // nolint: staticcheck
-	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/known/structpb"
-	networking "istio.io/api/networking/v1alpha3"
+	"github.com/dubbo-go-pixiu/pixiu-api/pkg/xds"
+	"github.com/mitchellh/mapstructure"
 )
 
 import (
-	"github.com/apache/dubbo-go-pixiu/pkg/util/protomarshal"
+	"github.com/apache/dubbo-go-pixiu/pkg/common/constant"
+	"github.com/apache/dubbo-go-pixiu/pkg/config/xds/apiclient"
+	"github.com/apache/dubbo-go-pixiu/pkg/logger"
+	"github.com/apache/dubbo-go-pixiu/pkg/model"
+	"github.com/apache/dubbo-go-pixiu/pkg/server/controls"
 )
 
-// nolint: interfacer
-func BuildXDSObjectFromStruct(applyTo networking.EnvoyFilter_ApplyTo, value *structpb.Struct, strict bool) (proto.Message, error) {
-	if value == nil {
-		// for remove ops
-		return nil, nil
+type (
+	DiscoverApi interface {
+		Fetch(localVersion string) ([]*apiclient.ProtoAny, error)
+		Delta() (chan *apiclient.DeltaResources, error)
 	}
-	var obj proto.Message
-	switch applyTo {
-	case networking.EnvoyFilter_CLUSTER:
-		obj = &cluster.Cluster{}
-	case networking.EnvoyFilter_LISTENER:
-		obj = &listener.Listener{}
-	case networking.EnvoyFilter_ROUTE_CONFIGURATION:
-		obj = &route.RouteConfiguration{}
-	case networking.EnvoyFilter_FILTER_CHAIN:
-		obj = &listener.FilterChain{}
-	case networking.EnvoyFilter_HTTP_FILTER:
-		obj = &httpConn.HttpFilter{}
-	case networking.EnvoyFilter_NETWORK_FILTER:
-		obj = &listener.Filter{}
-	case networking.EnvoyFilter_VIRTUAL_HOST:
-		obj = &route.VirtualHost{}
-	case networking.EnvoyFilter_HTTP_ROUTE:
-		obj = &route.Route{}
-	case networking.EnvoyFilter_EXTENSION_CONFIG:
-		obj = &core.TypedExtensionConfig{}
-	case networking.EnvoyFilter_BOOTSTRAP:
-		obj = &bootstrapv3.Bootstrap{}
+
+	AdapterConfig struct {
+	}
+
+	Xds struct {
+		//ads    DiscoverApi //aggregate discover service manager todo to implement
+		cds               *CdsManager //cluster discover service manager
+		lds               *LdsManager //listener discover service manager
+		exitCh            chan struct{}
+		listenerMg        controls.ListenerManager
+		clusterMg         controls.ClusterManager
+		dynamicResourceMg controls.DynamicResourceManager
+	}
+)
+
+func (a *Xds) createApiManager(config *model.ApiConfigSource,
+	node *model.Node,
+	resourceType apiclient.ResourceTypeName) DiscoverApi {
+	if config == nil {
+		return nil
+	}
+
+	switch config.APIType {
+	case model.ApiTypeGRPC:
+		return apiclient.CreateGrpExtensionApiClient(config, node, a.exitCh, resourceType)
+	case model.ApiTypeIstioGRPC:
+		dubboServices, err := a.readDubboServiceFromListener()
+		if err != nil {
+			logger.Errorf("can not read listener. %v", err)
+			return nil
+		}
+		return apiclient.CreateEnvoyGrpcApiClient(config, node, a.exitCh, resourceType, apiclient.WithIstioService(dubboServices...))
 	default:
-		return nil, fmt.Errorf("Envoy filter: unknown object type for applyTo %s", applyTo.String()) // nolint: stylecheck
+		logger.Errorf("un-support the api type %s", config.APITypeStr)
+		return nil
 	}
-
-	if err := StructToMessage(value, obj, strict); err != nil {
-		return nil, fmt.Errorf("Envoy filter: %v", err) // nolint: stylecheck
-	}
-	return obj, nil
 }
 
-func StructToMessage(pbst *structpb.Struct, out proto.Message, strict bool) error {
-	if pbst == nil {
-		return errors.New("nil struct")
+func (a *Xds) readDubboServiceFromListener() ([]string, error) {
+	dubboServices := make([]string, 0)
+	listeners, err := a.listenerMg.CloneXdsControlListener()
+	if err != nil {
+		return nil, err
 	}
 
-	buf := &bytes.Buffer{}
-	if err := (&jsonpb.Marshaler{OrigName: true}).Marshal(buf, pbst); err != nil {
-		return err
+	for _, l := range listeners {
+		for _, filter := range l.FilterChain.Filters {
+			if filter.Name != constant.HTTPConnectManagerFilter {
+				continue
+			}
+			var cfg *model.HttpConnectionManagerConfig
+			if filter.Config != nil {
+				if err := mapstructure.Decode(filter.Config, &cfg); err != nil {
+					logger.Error("read listener config error when init xds", err)
+					continue
+				}
+			}
+			for _, httpFilter := range cfg.HTTPFilters {
+				if httpFilter.Name == constant.HTTPDirectDubboProxyFilter {
+					for _, route := range cfg.RouteConfig.Routes {
+						dubboServices = append(dubboServices, route.Route.Cluster)
+					}
+				}
+			}
+		}
+	}
+	return dubboServices, nil
+}
+
+func (a *Xds) Start() {
+	if a.dynamicResourceMg == nil { // if dm is nil, then config not initialized.
+		logger.Infof("can not get dynamic resource manager. maybe the config has not initialized")
+		return
+	}
+	apiclient.Init(a.clusterMg)
+
+	// lds fetch just run on init phase.
+	if a.dynamicResourceMg.GetLds() != nil {
+		a.lds = &LdsManager{
+			DiscoverApi: a.createApiManager(a.dynamicResourceMg.GetLds(), a.dynamicResourceMg.GetNode(), xds.ListenerType),
+			listenerMg:  a.listenerMg,
+		}
+		if err := a.lds.Delta(); err != nil {
+			logger.Errorf("can not fetch lds err is %+v", err)
+		}
+	}
+	// catch the ongoing cds config change.
+	if a.dynamicResourceMg.GetCds() != nil {
+		a.cds = &CdsManager{
+			DiscoverApi: a.createApiManager(a.dynamicResourceMg.GetCds(), a.dynamicResourceMg.GetNode(), xds.ClusterType),
+			clusterMg:   a.clusterMg,
+		}
+		if err := a.cds.Delta(); err != nil {
+			logger.Errorf("can not fetch lds")
+		}
 	}
 
-	// If strict is not set, ignore unknown fields as they may be sending versions of
-	// the proto we are not internally using
-	if strict {
-		return protomarshal.Unmarshal(buf.Bytes(), out)
-	}
-	return protomarshal.UnmarshalAllowUnknown(buf.Bytes(), out)
+}
+
+func (a *Xds) Stop() {
+	apiclient.Stop()
+	close(a.exitCh)
+}
+
+var (
+	client Client
+	once   sync.Once
+)
+
+// Client xds client
+type Client interface {
+	Stop()
+}
+
+// StartXdsClient create XdsClient and run. only one xds client create at first(singleton)
+func StartXdsClient(listenerMg controls.ListenerManager, clusterMg controls.ClusterManager, drm controls.DynamicResourceManager) Client {
+	once.Do(func() {
+		xdsClient := &Xds{
+			listenerMg:        listenerMg,
+			clusterMg:         clusterMg,
+			dynamicResourceMg: drm,
+			exitCh:            make(chan struct{}),
+		}
+		xdsClient.Start()
+		client = xdsClient
+	})
+
+	return client
 }
